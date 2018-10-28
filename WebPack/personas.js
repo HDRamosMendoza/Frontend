30b49a1e970cf257ac67c3e13648f3ab263c431c
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2018-10-27 23:30:00 -0500

    Actualizando pruebas

--- WebPack/personas.js
+++ WebPack/personas.js
@@ -1,1 +1,24 @@
-module.export = "Hola Mundo";
+function hola() {
+    return "Hola mundo";
+}
+module.exports = hola;
+/*
+module.export = {
+    name: "Hola",
+    mundo: "mensaje"
+};
+*/
+
+/*
+var personas = [
+    {
+        name: 'jesus',
+        nickname: 'jesus123'
+    },
+    {
+        name: 'daniel',
+        nickname: 'soy tu padre'
+    }
+
+]
+*/
