f9d58825d8a355baa47db52079289c361d979556
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2018-10-30 21:52:17 -0500

    ReactJS Hola mundo

---
+++ ReactJS/Projects/hello-world/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+//Todos los componentes necesitan un metodo RENDER
+//React trabaja con un jsx. Una especie de html en donde podemos
+//ejecutar componentes.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
