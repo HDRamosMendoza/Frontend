1ce20ba6bfff1599b01e3c92d12589f99ae00f27
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2019-08-27 19:08:52 -0500

    Se cambio el origen de la carpeta

--- WebPack/app.js
+++
@@ -1,9 +0,0 @@
-var personas = require('./personas.js');
-var $ = require(jquery);
-
-$('body').append('<h1>' + personas[0].name + '</h1>');
-    
-
-console.log(personas);
-//var mensaje = require('./personas.js');
-//alert(mensaje.name);
